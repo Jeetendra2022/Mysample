d5b8cd39055364bbb9dc2ef14f1646bad11a9744
jeetendra.navgikar <jeetendra.navgikar@globant.com>
2023-10-05 16:30:01 -0300

    first commit

---
+++ src/test/java/org/example/Sample.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Sample {
+
+
+   @Test
+    public void test1(){
+        System.out.println("My test 1");
+    }
+
+   @Test
+    public void test2(){
+        System.out.println("My test 2");
+    }
+
+
+    @Test
+    public void test3(){
+        System.out.println("My test 3");
+    }
+
+
+   @Test
+    public void test4(){
+        System.out.println("my test 4");
+    }
+
+}
